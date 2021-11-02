ab8fc1547ddbfb67352b6fe037e0887742f5f1bd
yang <yang@getblueshift.com>
2021-11-01 21:36:17 -0700

    debug

--- lib/pulsar_ex/consumer.ex
+++ lib/pulsar_ex/consumer.ex
@@ -819,6 +819,7 @@ defmodule PulsarEx.Consumer do
                 |> Bitset.and_set(Bitset.from_words(message.ack_set, batch_size))
 
               if bitset == Bitset.new(batch_size) do
+                Logger.warn("Completing batch #{inspect(message_id)}")
                 true
               else
                 Bitset.to_words(bitset)
@@ -835,7 +836,7 @@ defmodule PulsarEx.Consumer do
         %{state | acks: acks}
       end
 
-      defp track_nack(%{message_id: message_id}, state) do
+      defp track_nack(%{message_id: message_id, redelivery_count: redelivery_count}, state) do
         resend_ts =
           case state.redelivery_policy do
             :exp ->
