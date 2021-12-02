05b7504d3a96b049af19ab61687f6262a01a38a4
yang <yang@getblueshift.com>
2021-12-01 18:08:37 -0800

    create producer behaviour for testing

--- lib/pulsar_ex/callbacks.ex
+++ lib/pulsar_ex/callbacks.ex
@@ -11,3 +11,14 @@ defmodule PulsarEx.WorkerCallback do
   @callback partition_key(job :: atom(), params :: any(), message_opts :: keyword()) :: String.t()
   @callback handle_job(job :: atom(), params :: any()) :: :ok | {:ok, term()} | {:error, term()}
 end
+
+defmodule PulsarEx.ProducerCallback do
+  alias PulsarEx.Proto.{MessageIdData, ServerError}
+
+  @callback produce(
+              topic_name :: String.t(),
+              payload :: binary(),
+              message_opts :: keyword(),
+              producer_opts :: keyword()
+            ) :: {:ok, MessageIdData.t()} | {:error, ServerError.t()}
+end
