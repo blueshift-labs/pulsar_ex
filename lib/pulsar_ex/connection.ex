385a207b3bb52671ea5ab9d69ef278bb123b60c5
yang <yang@getblueshift.com>
2023-01-09 17:10:44 -0800

    catch all exit

--- lib/pulsar_ex/connection.ex
+++ lib/pulsar_ex/connection.ex
@@ -75,12 +75,16 @@ defmodule PulsarEx.Connection do
   def create_producer(conn, topic, opts \\ []) do
     GenServer.call(conn, {:create_producer, topic, opts}, @request_timeout)
   catch
-    :exit, {:timeout, _} ->
+    :exit, err ->
       :telemetry.execute(
         [:pulsar_ex, :producer, :timeout],
         %{count: 1},
         %{}
       )
+
+      Rollbax.report_message(:error, "exit create producer", %{
+        error: inspect(err)
+      })
 
       {:error, :timeout}
   end
